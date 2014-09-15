6bfcca69702edebf5766b7946ad140059b1310ed
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2014-09-15 11:13:04 +0200

    fix typo in description

--- stock_reserve/__openerp__.py
+++ stock_reserve/__openerp__.py
@@ -38,7 +38,7 @@ Each reservation can have a validity date, once passed, the reservation
 is automatically lifted.
 
 The reserved products are substracted from the virtual stock. It means
-that if you reserved a quantity of products which bring the virtual
+that if you reserved a quantity of product which brings the virtual
 stock below the minimum, the orderpoint will be triggered and new
 purchase orders will be generated. It also implies that the max may be
 exceeded if the reservations are canceled.
